06da9d038becd6a7fd94e79fcae41d67bdb09693
obedgyamfi <obedgyamfi53@gmail.com>
2023-07-13 19:00:51 +0000

    changed: Get Started -> Sign Up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,13 +171,13 @@ export default function Home() {
                   href="#"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Get started
+                  Sign Up
                 </a>
                 <a
                   href="#"
                   className="text-sm font-semibold leading-6 text-gray-200"
                 >
-                  Learn more <span aria-hidden="true">→</span>
+                  Log in <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
